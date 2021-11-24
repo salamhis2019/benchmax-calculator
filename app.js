1f50408ed86027d53c00a8905d3fa6b5f9b28618
salamhis2019 <salamehh@msu.edu>
2021-11-24 00:34:41 -0500

    js updated and trash icon updated

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+// COURSE OF ACTION: 
+// BEGIN BY DEFINING UI ELEMENTS WITHIN THE DOM 
+// OBTAIN FORMULA FOR CONVERSION FROM REPITIONS AND WEIGHT PROVIDED
+// CREATE A FUNCTION WHERE THE CONVERESION FROM THE GIVEN USER INPUT VALUES CALCULATED
+// GET ICONS FROM BOOTSTRAP/W3S 
+// CREATE CONTAINER WHERE RESULTS ARE APPENDED AND DISPLAYED TO THE USER
+// CREATE A TRASH ICON WHERE THE USER CAN REMOVE THEIR RESULTS
+// CREATE A NOTE AREA WHERE THE USER KNOWS HOW TO CONVERT FROM KG TO LBPS
+// ALLOW THE USER TO SWITCH BETWEEN KG AND LBPS REPEATEDLY
+
+
+// CALCULATE BUTTON
 const button = document.getElementById('button');
 
 button.addEventListener('click', calculateMax);
 
+// BENCH MAX CALCULATOR
 function calculateMax(e) {
+
   e.preventDefault();
 
-    // DEFLINE UI ELEMENTS
+    // DEFLINING UI/DOM ELEMENTS
     const weight = document.getElementById('weight').value;
     const reps = document.getElementById('reps').value;
     const results = document.getElementById('results');
     const errorContainer = document.getElementById('error-container');
 
-    // ERROR SECTION
+    // ERROR DISPLAY
     if(reps.length === 0 && weight.length === 0) {
         showError('ENTER CALCULATED VALUES!');
     } else {
+    
     // REPITIONS EQUATION
     var repitions = parseFloat((reps/30) + 1);
 
@@ -24,16 +39,14 @@ function calculateMax(e) {
     results.classList.add('result');
     weightList.classList.add('text-hover', 'lb')
     results.appendChild(weightList);
+    
     // DELETE ICON
     var trashIcon = document.createElement('i');
     trashIcon.classList.add('trash-icon');
     trashIcon.innerHTML = '<i class="fa fa-trash"></i>';
     results.appendChild(trashIcon);
     }
-
-    // weightList.addEventListener('click', function(){
-    //   weightList.innerText = ((weight * repitions) / 2.205).toFixed(1) + ' KG';
-    // })
+    // CONVERT FROM KG TO LBPS BACK AND FORTH
     weightList.addEventListener('click', function(){
       if(weightList.classList.contains('lb')){
         weightList.innerText = ((weight * repitions) / 2.205).toFixed(1) + ' KG';
@@ -44,36 +57,32 @@ function calculateMax(e) {
       }
     })
 
-    // weightList.addEventListener('dblclick', function(){
-    //     results.removeChild(weightList);
-    //     results.classList.remove('result');
-    // })
-
     trashIcon.addEventListener('click', function() {
         results.removeChild(weightList);
         results.removeChild(trashIcon);
         results.classList.remove('result');
     })
+  
+  };
 
-    saveResults(weightList.value);
-};
-
-// Show Error
+// FUNCTION SHOW ERROR TO USER
 function showError(error){
-  // Create a div
+  // CREATE DIV FOR ERROR
   const errorDiv = document.createElement('div');
 
-  // Get elements
+  // GET ELEMENTS FOR ERROR CONTAINER
   const container = document.querySelector('#container');
   const heading = document.querySelector('.heading');
 
+  // ERROR MESSAGE FORMATTING
   errorDiv.className = 'alert alert-danger';
-    errorDiv.appendChild(document.createTextNode(error));
-  container.insertBefore(errorDiv, heading);
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // CLEAR ERROR MESSAGE AUTOSMATICALLY IN THREE SECONDS
   setTimeout(clearError, 3000);
 }
 
-// Clear error
+// CLEAR ERROR FUNCTION FOR CALL BACK IN THE MAX CALCULATORS
 function clearError(){
-  document.querySelector('.alert').remove();
+  document.querySelector('.alSert').remove();
 }
