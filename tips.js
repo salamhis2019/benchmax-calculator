2accc013713933c9a593cb5fd001e602c4757043
salamhis2019 <salamehh@msu.edu>
2021-12-04 01:57:48 -0500

    hamburger menu and tips responsiveness complete

---
+++ tips.js
@@ -0,0 +1,20 @@
+// DEFINE UI VARS
+const hamburger = document.querySelector('.fa-align-justify');
+const links = document.querySelector('.links')
+
+// ADD EVENT LISTENER TO HAMBURGER ICON
+hamburger.addEventListener('click', showMenu)
+
+// SHOW MENU FUNCTION
+function showMenu() {
+    // REVEAL THE NAV MENU
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links');
+        hamburger.classList.add('fa-align-justify')
+    // REMOVE HAMBURGER AND ADD 'X'
+    } else {
+        links.classList.add('show-links');
+        hamburger.classList.remove('fa-align-justify');
+        hamburger.classList.add('fa-remove')
+    }
+}
